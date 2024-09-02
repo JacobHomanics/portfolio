3f82b98cad47ac11e1e5b47c167a8393c89d1ebb
Jacob Homanics <homanicsjake@gmail.com>
2024-09-02 08:56:31 -0500

    fixed links

--- packages/nextjs/components/Project.tsx
+++ packages/nextjs/components/Project.tsx
@@ -6,6 +6,7 @@ type Props = {
   name: string;
   description: string;
   img: any;
+  url: string;
 };
 
 // const textExample =
@@ -14,18 +15,18 @@ type Props = {
 
 // const shortTextExample = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
 
-export const Project = ({ name, description, img }: Props) => {
+export const Project = ({ name, description, img, url }: Props) => {
   return (
     <div className="flex bg-primary p-4 rounded-lg border-2 border-indigo-500 items-start">
-      <Link href={"#"} className="w-[100px] h-[100px] flex-none">
+      <Link href={url} target="#" className="w-[100px] h-[100px] flex-none">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src={img.src} alt="Hejh" className="w-full h-full object-cover rounded-lg" />
       </Link>
       <div className="flex flex-col pl-6 items-start">
         {/* <Link href={"#"} className="container"> */}
-        <a href="#" className="">
+        <Link href={url} target="#">
           <div className="text-2xl text-blue-600 dark:text-blue-500 hover:underline">{name}</div>
-        </a>
+        </Link>
         {/* </Link> */}
         <div className="break-all">{description}</div>
       </div>
---
+++ packages/nextjs/components/jacobhomanics/UniversalIcon.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+// import Link from "next/link";
+import { IconLink } from "./IconLink";
+
+type Props = {
+  items?: any[];
+  size?: "sm" | "base" | "lg";
+};
+
+// const sizeMap = {
+//   sm: 7,
+//   base: "w-[75px] lg:w-[275px]",
+//   lg: "",
+// };
+
+export const SocialLinks = ({
+  items,
+}: //   size = "base",
+Props) => {
+  const itemsElements = items?.map((item, index) => {
+    return (
+      <div
+        key={"socialLinks-" + index}
+        className={
+          item.type === "component" ? "m-1 hover:brightness-200 dark:hover:brightness-75" : "m-1 hover:brightness-75"
+        }
+      >
+        <IconLink url={item.url}>
+          {item.type === "img" ? (
+            /* eslint-disable-next-line @next/next/no-img-element */
+            <img src={item.img.src} alt="Link" className="w-min h-7 lg:h-10" />
+          ) : (
+            <div className="">
+              <item.img className="w-7 h-7 lg:w-8 lg:h-8" />
+            </div>
+          )}
+        </IconLink>
+      </div>
+    );
+  });
+
+  return <div className="flex flex-wrap items-center justify-start">{itemsElements}</div>;
+};
