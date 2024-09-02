a25eec0a871c6d899676713002d4344a37210153
Jacob Homanics <homanicsjake@gmail.com>
2024-09-02 08:39:04 -0500

    progress 2

--- packages/nextjs/components/Project.tsx
+++ packages/nextjs/components/Project.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 
 type Props = {
+  name: string;
+  description: string;
   img: any;
 };
 
@@ -10,22 +12,22 @@ type Props = {
 //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at risus velit. Integer porta lectus ac nisi semper, a vehicula lectus iaculis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce vitae sollicitudin dui. Integer scelerisque a purus eget hendrerit. Nam sagittis elit quis fermentum tempus. Aliquam non venenatis ante, eget efficitur tortor. Maecenas lorem felis, ullamcorper ac volutpat et, fringilla ac est. Donec scelerisque purus magna, id volutpat mauris feugiat id. Phasellus dignissim sed ex sed porttitor. Ut fringilla accumsan lectus, vel aliquet quam laoreet vitae. Duis eros massa, dictum pellentesque nisl vitae, malesuada rhoncus neque." +
 //   "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec ultrices ex. Pellentesque accumsan varius odio non ullamcorper. Vivamus blandit ligula a aliquet imperdiet. Donec sapien purus, rutrum facilisis maximus nec, eleifend vel tortor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut hendrerit semper mauris. Integer sed dictum augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.";
 
-const shortTextExample = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+// const shortTextExample = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
 
-export const Project = ({ img }: Props) => {
+export const Project = ({ name, description, img }: Props) => {
   return (
-    <div className="flex bg-primary p-4 rounded-lg border-2 border-indigo-500 items-center md:items-start">
+    <div className="flex bg-primary p-4 rounded-lg border-2 border-indigo-500 items-start">
       <Link href={"#"} className="w-[100px] h-[100px] flex-none">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src={img.src} alt="Hejh" className="w-full h-full object-cover rounded-lg" />
       </Link>
-      <div className="flex flex-col pl-6">
+      <div className="flex flex-col pl-6 items-start">
         {/* <Link href={"#"} className="container"> */}
-        <a href="#">
-          <div className="text-2xl text-blue-600 dark:text-blue-500 hover:underline">Ladders.Vision</div>
+        <a href="#" className="">
+          <div className="text-2xl text-blue-600 dark:text-blue-500 hover:underline">{name}</div>
         </a>
         {/* </Link> */}
-        <div className="break-all">{shortTextExample}</div>
+        <div className="break-all">{description}</div>
       </div>
     </div>
   );
