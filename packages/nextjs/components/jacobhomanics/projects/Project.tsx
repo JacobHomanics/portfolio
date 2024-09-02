a63d88fd17f7f3f618849acdffb97c0184bb6650
Jacob Homanics <homanicsjake@gmail.com>
2024-09-02 15:44:20 -0500

    click to show more

--- packages/nextjs/components/jacobhomanics/projects/Project.tsx
+++ packages/nextjs/components/jacobhomanics/projects/Project.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 
 type Props = {
@@ -10,6 +11,17 @@ type Props = {
 };
 
 export const Project = ({ name, description, img, url }: Props) => {
+  const [isLineClamped, setIsLineClamped] = useState(true);
+  const [isClamped, setIsClamped] = useState(false);
+  const descriptionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const element = descriptionRef.current;
+    if (element) {
+      setIsClamped(element.scrollHeight > element.clientHeight);
+    }
+  }, [description]);
+
   return (
     <div className="flex bg-secondary p-4 rounded-lg border-2 border-indigo-500 items-start">
       <Link href={url} target="#" className="w-[100px] h-[100px] flex-none">
@@ -20,7 +32,20 @@ export const Project = ({ name, description, img, url }: Props) => {
         <Link href={url} target="#">
           <div className="text-2xl text-blue-600 dark:text-blue-500 hover:underline">{name}</div>
         </Link>
-        <div className="break-all">{description}</div>
+        <div ref={descriptionRef} className={`break-all ${isLineClamped ? "line-clamp-2" : ""}`}>
+          {description}
+        </div>
+
+        {isClamped && (
+          <button
+            onClick={() => {
+              setIsLineClamped(!isLineClamped);
+            }}
+            className="hover:underline cursor-pointer"
+          >
+            {isLineClamped ? "Click to show more." : "Click to show less."}
+          </button>
+        )}
       </div>
     </div>
   );
